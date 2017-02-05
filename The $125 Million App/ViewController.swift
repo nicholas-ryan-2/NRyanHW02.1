af2dd5506d4ce719257738c012340e27a17fe34e
Nick <nickryan@nicks-air.bc.edu>
2017-02-05 15:19:41 -0500

    3.1 Commits

--- The $125 Million App/ViewController.swift
+++ The $125 Million App/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var conversionMessage: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func convertButton(_ sender: UIButton) {
+        let miles = Double(textInput.text!)
+        if let miles = miles {
+            let km = miles * 1.6
+            conversionMessage.text = "\(miles) miles is \(km) kilometers."
+        } else {
+            conversionMessage.text = "Error: \(miles!) is not a valid input."
+        }
 }
-
+}
