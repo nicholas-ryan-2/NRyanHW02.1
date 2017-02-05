3fd5686105945fbc01c5c8480ad26d2a20d1f2d0
Nick <nickryan@Nicks-MacBook-Air.local>
2017-02-05 14:03:27 -0500

    Initial Commit

---
+++ The $125 Million AppTests/The__125_Million_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  The__125_Million_AppTests.swift
+//  The $125 Million AppTests
+//
+//  Created by Nick on 2/5/17.
+//  Copyright © 2017 Nick Ryan. All rights reserved.
+//
+
+import XCTest
+@testable import The__125_Million_App
+
+class The__125_Million_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
